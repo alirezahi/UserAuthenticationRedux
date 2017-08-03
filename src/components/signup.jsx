30bea09aa7462c7cc92b03f83025b8c110be5a16
Alireza <alireza97hi@gmail.com>
2017-08-03 18:59:05 +0430

    sign up page changed a little

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 class Signup extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
     render(){
+        console.log(this.state);
         return(
-            <div>signup</div>
+            <div className="form-inline">
+                <h2></h2>
+                <div>
+                    <input 
+                    className="form-control"
+                    type="text"
+                    placeholder="Username"
+                    onChange={event => this.setState({email:event.target.value})}
+                    />
+                    <input 
+                    className="form-control"
+                    type="password"
+                    placeholder="password"
+                    onChange={event => this.setState({password:event.target.value})}
+                    />
+                    <button
+                    className="btn btn-success" type="button">Sign Up
+                    </button>
+                </div>
+                
+            </div>
         )
     }
 }
